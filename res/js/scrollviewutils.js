195d44808f6d1d2110d782b6e179b5a286c693c4
BearHubs <bear90789078@gmail.com>
2023-12-15 09:20:55 +0800

    1215

--- res/js/scrollviewutils.js
+++ res/js/scrollviewutils.js
@@ -8,12 +8,29 @@
 var scrollviewutils = (function () {
     return {
         ito: ito,
-        cto: cto
+        cto: cto,
+        sie: sie
     };
     function ito(id) {
         document.getElementById(id).scrollIntoView();
     }
     function cto(c) {
         document.querySelector(c).scrollIntoView();
+    }
+    function sie(id) {
+        const element = document.querySelector(id);
+
+        if (!element) {
+            return false;
+        }
+
+        const windowHeight = window.innerHeight;
+        const elementRect = element.getBoundingClientRect();
+        const elementTop = elementRect.top;
+        const elementBottom = elementRect.bottom;
+
+        const isPartiallyInView = (elementTop <= windowHeight && elementBottom >= 0);
+
+        return isPartiallyInView;
     }
 }());
