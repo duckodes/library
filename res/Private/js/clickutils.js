08e081ba9276ea31e8f4b4754c6590cc6f227821
BearHubs <bear90789078@gmail.com>
2024-01-11 11:24:08 +0800

    20240111

--- res/Private/js/clickutils.js
+++ res/Private/js/clickutils.js
@@ -1,11 +1,13 @@
 const clickutils = (function () {
     return {
         click: click,
-        nClick: nClick
+        nClick: nClick,
+        touch: touch,
+        nTouch: nTouch
     };
     function click(element, button, func) {
         let start = false;
-        element.addEventListener("mousedown", (e) => {
+        function mouseDown(e) {
             if (e.button === button) {
                 start = true;
             } else {
@@ -14,36 +16,24 @@ const clickutils = (function () {
             function mouseUp() {
                 start = false;
                 window.removeEventListener('mouseup', mouseUp);
+                element.removeEventListener('mousedown', mouseDown);
             }
             window.addEventListener('mouseup', mouseUp);
-        });
+        }
+        element.addEventListener("mousedown", mouseDown);
         function mouseUp(e) {
             if (start) {
                 func(e);
             }
             element.removeEventListener('mouseup', mouseUp);
+            element.removeEventListener('mousedown', mouseDown);
         }
         element.addEventListener("mouseup", mouseUp);
-        element.addEventListener("touchstart", (e) => {
-            start = true;
-            function touchEnd() {
-                start = false;
-                window.removeEventListener('touchmove', touchEnd);
-            }
-            window.addEventListener('touchmove', touchEnd);
-        });
-        function touchEnd(e) {
-            if (start) {
-                func(e);
-            }
-            element.removeEventListener('touchend', touchEnd);
-        }
-        element.addEventListener("touchend", touchEnd);
     }
     function nClick(element, button) {
         return new Promise((resolve) => {
             let start = false;
-            element.addEventListener("mousedown", (e) => {
+            function mouseDown(e) {
                 if (e.button === button) {
                     start = true;
                 } else {
@@ -52,29 +42,61 @@ const clickutils = (function () {
                 function mouseUp() {
                     start = false;
                     window.removeEventListener('mouseup', mouseUp);
+                    element.removeEventListener('mousedown', mouseDown);
                 }
                 window.addEventListener('mouseup', mouseUp);
-            });
+            }
+            element.addEventListener("mousedown", mouseDown);
             function mouseUp(e) {
                 if (start) {
                     resolve(e);
                 }
                 element.removeEventListener('mouseup', mouseUp);
+                element.removeEventListener('mousedown', mouseDown);
             }
             element.addEventListener("mouseup", mouseUp);
-            element.addEventListener("touchstart", () => {
+        });
+    }
+    function touch(element, func) {
+        let start = false;
+        function touchStart() {
+            start = true;
+            function touchEnd() {
+                start = false;
+                window.removeEventListener('touchmove', touchEnd);
+                element.removeEventListener("touchstart", touchStart);
+            }
+            window.addEventListener('touchmove', touchEnd);
+        }
+        element.addEventListener("touchstart", touchStart);
+        function touchEnd(e) {
+            if (start) {
+                func(e);
+            }
+            element.removeEventListener('touchend', touchEnd);
+            element.removeEventListener("touchstart", touchStart);
+        }
+        element.addEventListener("touchend", touchEnd);
+    }
+    function nTouch(element) {
+        return new Promise((resolve) => {
+            let start = false;
+            function touchStart() {
                 start = true;
                 function touchEnd() {
                     start = false;
                     window.removeEventListener('touchmove', touchEnd);
+                    element.removeEventListener('touchstart', touchStart);
                 }
                 window.addEventListener('touchmove', touchEnd);
-            });
+            }
+            element.addEventListener("touchstart", touchStart);
             function touchEnd(e) {
                 if (start) {
                     resolve(e);
                 }
                 element.removeEventListener('touchend', touchEnd);
+                element.removeEventListener('touchstart', touchStart);
             }
             element.addEventListener("touchend", touchEnd);
         });
